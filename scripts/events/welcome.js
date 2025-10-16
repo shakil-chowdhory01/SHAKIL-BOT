b6d104d9a85a62b2609237cfb323f308c4a783ab
jerraislam6-droid <jerraislam6@gmail.com>
2025-10-16 18:47:53 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,35 +1,53 @@
 const { getTime, drive } = global.utils;
+
 if (!global.temp.welcomeEvent)
 	global.temp.welcomeEvent = {};
 
 module.exports = {
 	config: {
 		name: "welcome",
-		version: "1.7",
-		author: "NTKhang",
+		version: "2.0",
+		author: "Mehedi Hassan",
 		category: "events"
 	},
 
 	langs: {
-		vi: {
-			session1: "sáng",
-			session2: "trưa",
-			session3: "chiều",
-			session4: "tối",
-			welcomeMessage: "Cảm ơn bạn đã mời tôi vào nhóm!\nPrefix bot: %1\nĐể xem danh sách lệnh hãy nhập: %1help",
-			multiple1: "bạn",
-			multiple2: "các bạn",
-			defaultWelcomeMessage: "Xin chào {userName}.\nChào mừng bạn đến với {boxName}.\nChúc bạn có buổi {session} vui vẻ!"
-		},
-		en: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
-			multiple1: "you",
-			multiple2: "you guys",
-			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊`
+		bn: {
+			session1: "সকাল",
+			session2: "দুপুর",
+			session3: "বিকেল",
+			session4: "রাত",
+
+			// যখন বট নিজে অ্যাড হয়
+			welcomeMessage: `
+━━━━━━━━━━━━━━━━━━━━━
+🤖 ধন্যবাদ আমাকে এই গ্রুপে অ্যাড করার জন্য 💖
+
+⚙️ Bot Prefix :  %1
+📜 সব কমান্ড দেখতে লিখুন :  %1help
+
+চলুন একসাথে এই গ্রুপটা আরও মজার করে তুলি! 😄
+
+👑 Bot Owner : Mohammad Akash
+━━━━━━━━━━━━━━━━━━━━━
+			`,
+
+			// যখন নতুন মেম্বার যোগ হয়
+			defaultWelcomeMessage: `
+__আসসালামু আলাইকুম__
+═══════════════
+__Welcome ➤ {userName}__
+
+_আমাদের {boxName}_
+_এর পক্ষ থেকে আপনাকে_
+       __!! স্বাগতম !!__
+__আপনি এই__
+        __গ্রুপের {memberCount} নাম্বার মেম্বার__!!
+
+___Added By : {inviterName}___
+
+Bot Owner : Mohammad Akash
+			`
 		}
 	},
 
@@ -38,93 +56,72 @@ module.exports = {
 			return async function () {
 				const hours = getTime("HH");
 				const { threadID } = event;
-				const { nickNameBot } = global.GoatBot.config;
 				const prefix = global.utils.getPrefix(threadID);
+				const { nickNameBot } = global.GoatBot.config;
 				const dataAddedParticipants = event.logMessageData.addedParticipants;
-				// if new member is bot
-				if (dataAddedParticipants.some((item) => item.userFbId == api.getCurrentUserID())) {
+
+				// 🟢 যদি বট নিজে অ্যাড হয়
+				if (dataAddedParticipants.some((i) => i.userFbId == api.getCurrentUserID())) {
 					if (nickNameBot)
 						api.changeNickname(nickNameBot, threadID, api.getCurrentUserID());
 					return message.send(getLang("welcomeMessage", prefix));
 				}
-				// if new member:
+
+				// 🟢 নতুন মেম্বারদের জন্য
 				if (!global.temp.welcomeEvent[threadID])
 					global.temp.welcomeEvent[threadID] = {
 						joinTimeout: null,
 						dataAddedParticipants: []
 					};
 
-				// push new member to array
 				global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...dataAddedParticipants);
-				// if timeout is set, clear it
 				clearTimeout(global.temp.welcomeEvent[threadID].joinTimeout);
 
-				// set new timeout
 				global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async function () {
 					const threadData = await threadsData.get(threadID);
 					if (threadData.settings.sendWelcomeMessage == false)
 						return;
+
 					const dataAddedParticipants = global.temp.welcomeEvent[threadID].dataAddedParticipants;
-					const dataBanned = threadData.data.banned_ban || [];
-					const threadName = threadData.threadName;
+					const threadName = threadData.threadName || "এই গ্রুপে";
 					const userName = [],
 						mentions = [];
-					let multiple = false;
-
-					if (dataAddedParticipants.length > 1)
-						multiple = true;
 
 					for (const user of dataAddedParticipants) {
-						if (dataBanned.some((item) => item.id == user.userFbId))
-							continue;
 						userName.push(user.fullName);
-						mentions.push({
-							tag: user.fullName,
-							id: user.userFbId
-						});
+						mentions.push({ tag: user.fullName, id: user.userFbId });
 					}
-					// {userName}:   name of new member
-					// {multiple}:
-					// {boxName}:    name of group
-					// {threadName}: name of group
-					// {session}:    session of day
+
 					if (userName.length == 0) return;
-					let { welcomeMessage = getLang("defaultWelcomeMessage") } =
-						threadData.data;
-					const form = {
-						mentions: welcomeMessage.match(/\{userNameTag\}/g) ? mentions : null
-					};
+
+					let { welcomeMessage = getLang("defaultWelcomeMessage") } = threadData.data;
+
+					// 🧮 মেম্বার কাউন্ট বের করা
+					const threadInfo = await api.getThreadInfo(threadID);
+					const memberCount = threadInfo.participantIDs.length;
+
+					// 🧍‍♂️ যিনি অ্যাড করেছেন
+					const inviterID = event.author;
+					let inviterName = "Unknown";
+					try {
+						const info = await api.getUserInfo(inviterID);
+						inviterName = info[inviterID]?.name || "Unknown";
+					} catch { }
+
+					let session;
+					if (hours <= 10) session = getLang("session1");
+					else if (hours <= 12) session = getLang("session2");
+					else if (hours <= 18) session = getLang("session3");
+					else session = getLang("session4");
+
 					welcomeMessage = welcomeMessage
-						.replace(/\{userName\}|\{userNameTag\}/g, userName.join(", "))
-						.replace(/\{boxName\}|\{threadName\}/g, threadName)
-						.replace(
-							/\{multiple\}/g,
-							multiple ? getLang("multiple2") : getLang("multiple1")
-						)
-						.replace(
-							/\{session\}/g,
-							hours <= 10
-								? getLang("session1")
-								: hours <= 12
-									? getLang("session2")
-									: hours <= 18
-										? getLang("session3")
-										: getLang("session4")
-						);
+						.replace(/\{userName\}/g, userName.join(", "))
+						.replace(/\{boxName\}/g, threadName)
+						.replace(/\{memberCount\}/g, memberCount)
+						.replace(/\{inviterName\}/g, inviterName)
+						.replace(/\{session\}/g, session);
 
-					form.body = welcomeMessage;
-
-					if (threadData.data.welcomeAttachment) {
-						const files = threadData.data.welcomeAttachment;
-						const attachments = files.reduce((acc, file) => {
-							acc.push(drive.getFile(file, "stream"));
-							return acc;
-						}, []);
-						form.attachment = (await Promise.allSettled(attachments))
-							.filter(({ status }) => status == "fulfilled")
-							.map(({ value }) => value);
-					}
-					message.send(form);
+					message.send({ body: welcomeMessage, mentions });
 					delete global.temp.welcomeEvent[threadID];
 				}, 1500);
 			};
